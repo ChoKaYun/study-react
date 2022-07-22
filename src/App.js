63aeea05f481d20bfa742514a9d24c92a162dbe5
ChoKaYun <kayun85@gmail.com>
2022-07-22 22:59:52 +0900

    [1주차 1-16장] state props, render 함수

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import Header from './components/Header';
+import Nav from './components/Nav';
+import Contents from './components/Contents';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mode: 'read',
+            welcom: {title: 'Welcom', desc: 'welcom contents'},
+            headerconts: {title: 'WEB', sub: 'world wide web'},
+            navlist: [
+                {id:1, title:'HTML', desc:'HTML is for information'},
+                {id:2, title:'CSS', desc:'HTML is for information'},
+                {id:3, title:'JavaScript', desc:'HTML is for information'}
+            ],
+            contents: {title:'HTML', desc: 'HTML is HyperText Markup Langguage'},
+        }
+    }
+    render() {
+        var _title, _desc = null;
+        if (this.state.mode === 'welcom') {
+            _title = this.state.welcom.title;
+            _desc = this.state.welcom.desc;
+        } else if (this.state.mode === 'read') {
+            _title = this.state.navlist[0].title;
+            _desc = this.state.navlist[0].desc;
+        }
+        return (
+            <div className="App">
+                {/* <Header title="WEB" sub="world wide web"></Header> */}
+                <header>
+                    <h1><a href="/" onClick={function(e){
+                        e.preventDefault();
+                        // this.state.mode = "welcom";
+                        this.setState({ mode: "welcom" });
+                    }.bind(this)}>{this.state.headerconts.title}</a></h1>
+                    <p>{this.state.headerconts.sub}</p>
+                </header>
+                <Nav data={this.state.navlist}></Nav>
+                <Contents
+                    title={_title}
+                    desc={_desc}>
+                </Contents>
+                <Contents desc="2번째 text만 하면?"></Contents>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <h1><a href="/">{this.props.title}</a></h1>
+                <p>{this.props.sub}</p>
+            </header>
+        );
+    }
+}
+
+export default Header;
