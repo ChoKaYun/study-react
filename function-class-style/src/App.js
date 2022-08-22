98101751ecf727972a57504a59be5fa3e7f570e4
ChoKaYun <kayun85@gmail.com>
2022-08-22 23:02:12 +0900

    [3주차 4-5장] 라이프사이클

--- function-class-style/src/App.js
+++ function-class-style/src/App.js
@@ -1,16 +1,26 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+    var [funcShow, setFuncShow] = useState(true);
+    var [classShow, setClassShow] = useState(true);
     return (
         <div className="container">
             <h1>Hi</h1>
+            <button type="button" onClick={function(){
+                setFuncShow(false);
+            }}>remove Func</button>
+            <button type="button" onClick={function(){
+                setClassShow(false);
+            }}>remove class</button>
             <FuncComp initNumber={1}></FuncComp>
             <ClassComp initNumber={1}></ClassComp>
         </div>
     );
 }
 
+var funcStyle = 'color:skyblue';
+var funcId = 0;
 function FuncComp(props) {
     var numberState = useState(props.initNumber);
     var number = numberState[0];
@@ -21,6 +31,30 @@ function FuncComp(props) {
     // var setDate = dateState[1];
     var [_date, setDate] = useState((new Date()).toString());
 
+    useEffect(function(){
+        console.log('%cfunc -> useEffect (componentDidMount)' +(++funcId), funcStyle);
+        document.title = number;
+        return function(){
+            console.log('%cfunc -> useEffect return (componentWillUnMount)' +(++funcId), funcStyle);
+        }
+    }, []);
+    // side Effect
+    useEffect(function(){
+        console.log('%cfunc -> useEffect number (componentDidMount & componentDidUpdate)' +(++funcId), funcStyle);
+        document.title = number;
+        return function(){
+            console.log('%cfunc -> useEffect return number (componentDidMount & componentDidUpdate)' +(++funcId), funcStyle);
+        }
+    }, [number]);
+    useEffect(function(){
+        console.log('%cfunc -> useEffect _date (componentDidMount & componentDidUpdate)' +(++funcId), funcStyle);
+        document.title = _date;
+        return function(){
+            console.log('%cfunc -> useEffect return _date (componentDidMount & componentDidUpdate)' +(++funcId), funcStyle);
+        }
+    }, [_date]);
+
+    console.log('%cfunc -> render' +(++funcId), funcStyle);
     return (
         <div className="container">
             <h2>Function</h2>
@@ -40,12 +74,33 @@ function FuncComp(props) {
     );
 }
 
+var classStyle = 'color:pink';
 class ClassComp extends React.Component {
     state = {
         number : this.props.initNumber,
         date : (new Date()).toString()
     }
+    componentWillMount() {
+        console.log('%cclass -> componentWillMount', classStyle);
+    }
+    componentDidMount() {
+        console.log('%cclass -> componentDidMount', classStyle);
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('%cclass -> shouldComponentUpdate', classStyle);
+        return true;
+    }
+    componentWillUpdate(nextProps, nextState) {
+        console.log('%cclass -> componentWillUpdate', classStyle);
+    }
+    componentDidUpdate(nextProps, nextState) {
+        console.log('%cclass -> componentDidUpdate', classStyle);
+    }
+    componentWillUnmount(nextProps, nextState) {
+        console.log('%cclass -> componentWillUnmount', classStyle);
+    }
     render() {
+        console.log('%cclass -> render', classStyle);
         return (
             <div className="container">
                 <h2>Class</h2>
